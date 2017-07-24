9703376bdb46bb323ceccea69f2d0bbb2bcc3f48
raynormw <tirtawiryaputra@yahoo.com>
2017-07-24 23:31:15 +0700

    add footer

--- bugtracker/src/App.js
+++ bugtracker/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import Header from './component/Header';
+import Footer from './component/Footer';
 import './App.css';
 
 class App extends React.Component {
@@ -9,7 +11,7 @@ class App extends React.Component {
         <div className="container">
           <Header />
         </div>
-
+        <Footer />
       </div>
     );
   }
